6325233a33d76fa84ce4ae7edeffc5053ba51385
Luissf1 <54382882+Luissf1@users.noreply.github.com>
2019-09-08 18:01:56 -0700

    Add files via upload

---
+++ Bici/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Bici
+{
+
+
+    class Bici 
+    {
+        //campos 
+        public string marca;
+        public string color;
+        public string type;
+        public int velocities;
+        public int size;
+
+        //metodos
+        public void print(){
+            Console.WriteLine("marca:{0},color:{1}",marca,color);
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Bici bici1 = new Bici();
+        bici1.marca = "BiciMex";
+        bici1.color="Rojo";
+        bici1.print();
+           // Console.WriteLine("Hello World!");
+        }
+    }
+}
