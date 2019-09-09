b6f77eb3ed9ebc980dd685f79aaf00c63331bf83
Luissf1 <54382882+Luissf1@users.noreply.github.com>
2019-09-08 18:05:06 -0700

    Add files via upload

---
+++ practica2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+namespace practica2
+{
+    class Program
+    {
+        static void Main()
+        {
+           List<String> lista =new List<String>();
+                lista.Add("Hola");
+                lista.Add("Hey");
+                lista.Add("Yes");
+
+           foreach(String s in lista)
+                Console.WriteLine(s);
+        }
+    }
+}
