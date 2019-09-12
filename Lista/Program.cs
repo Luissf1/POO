a5e035c55fdc12ca88a154bab9b9d15e1da2059b
Luissf1 <54382882+Luissf1@users.noreply.github.com>
2019-09-11 17:17:38 -0700

    Add files via upload

---
+++ Lista/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<String> lista =new List<String>();
+                lista.Add("1.El padrino");
+                lista.Add("2.Forrest Gump");
+                lista.Add("3.Coco");
+                lista.Add("4.El Rey León");
+                lista.Add("5.Toy Story");
+
+           foreach(String Pelicula in lista)
+                Console.WriteLine(Pelicula);
+        }
+    }
+}
