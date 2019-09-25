f9b46229aba8f5423a2252a98a12c9083862fc74
luissf1 <luis.silva1252@gmail.com>
2019-09-24 19:54:11 -0700

    practica

---
+++ espias/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace espias
+{
+
+    class Persona 
+    {
+       public string nombre;
+       public string apellido;
+
+       public Persona()
+       {
+           nombre="Fulano";
+           apellido="De tal";
+
+       }
+    }
+    class Anonymous{
+        public static void anonimiza (  Persona p)
+        {
+            p.nombre="xxxxx";
+            p.apellido="xxxxxx";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Persona p =new Persona();
+        Console.WriteLine(p.nombre);
+        Anonymous.anonimiza(p);
+        Console.WriteLine(p.nombre);
+        
+        }
+    }
+}
