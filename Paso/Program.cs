dbe51a10b02513a65d7cbb4c823b08f2dc0cf963
luissf1 <luis.silva1252@gmail.com>
2019-09-30 16:08:35 -0700

    tarea 5

---
+++ Paso/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Paso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
