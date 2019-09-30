9c0fc7414cf6d59ba043e3e9dfe302c24749a6b8
luissf1 <luis.silva1252@gmail.com>
2019-09-30 16:14:18 -0700

    update

--- Paso/Program.cs
+++ Paso/Program.cs
@@ -2,11 +2,65 @@
 
 namespace Paso
 {
-    class Program
+    
+    class Persona 
     {
-        static void Main(string[] args)
+       public string nombre;
+       public string apellido;
+
+       public Persona()
+       {
+           nombre="Fulano";
+           apellido="De tal";
+
+       }
+    }
+    class Anonymous{
+        public static void anonimiza (  Persona p)
         {
-            Console.WriteLine("Hello World!");
+            p.nombre="xxxxx";
+            p.apellido="xxxxxx";
+        }
+        public static void cambia(ref Persona p)
+        {
+            p = new Persona();
+            p.nombre="nuevo";
         }
     }
+
+    class Program
+    {
+        static void duplica(ref int x)
+        {
+            x = x * 2;
+        }
+        static int suma(int x,int y)
+        {
+            return x + y;
+        }
+         static void suma(int x,int y,out int resultado)
+        {
+            resultado = x + y;
+        }
+        static void Main(string[] args)
+        {
+            int a=2;
+            int b=3;
+            int r=suma(a,b);
+            int r2;
+            suma(a,b,out r2);
+
+
+            //duplica(ref a);
+            Console.WriteLine(r2);
+       /*  Persona p =new Persona();
+        Console.WriteLine(p.nombre);
+        Anonymous.anonimiza(p);
+        Anonymous.cambia(ref p);
+        Console.WriteLine(p.nombre);*/
+        
+        }
+    }
+
+
 }
