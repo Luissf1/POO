2ca58c410a2c92a8e8b338e23254952432920e6d
luissf1 <luis.silva1252@gmail.com>
2019-10-01 16:45:48 -0700

    trabajo clases

---
+++ gol/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace gol
+{
+    enum Estado{viva, muerta}
+
+    class Celula{
+        public bool estado_actual;
+        public bool estado_siguiente;
+        public Tablero tablero;
+        public short renglon;
+        public short columna;
+        
+
+        public Celula(Estado inicial,Tablero tablero, short renglon, short columna){
+            estado_actual = inicial;
+            this.tablero = tablero;
+            this.renglon = renglon;
+            this.columna = columna;
+        }
+    }
+
+    class Tablero{
+        private list <list<Celula>> grid;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("▒▒▒▒▒▒▒");
+            Console.WriteLine("▒▒▒▒▒▒▒");
+        }
+    }
+}
