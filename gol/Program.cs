b8a409d55251789c5b7585c9859d81df4ae54ca7
Luissf1 <luis.silva1252@gmail.com>
2019-10-04 19:35:15 -0700

    update

--- gol/Program.cs
+++ gol/Program.cs
@@ -3,81 +3,119 @@ using System.Collections.Generic;
 
 namespace gol
 {
-    enum Estado{viva, vacia}
-
-    class Celula{
-        public bool estado_actual;
-        public bool estado_siguiente;
+    enum Estado { viva, vacia}
+    class Celula {
+        public Estado estado_actual;
+        public Estado estado_siguiente;
         public Tablero tablero;
         public short renglon;
         public short columna;
         
-
-        public Celula(Estado inicial,Tablero tablero, short renglon, short columna){
+        public Celula(Estado inicial, Tablero tablero ,   short renglon, short columna)
+        {
             estado_actual = inicial;
             this.tablero = tablero;
             this.renglon = renglon;
             this.columna = columna;
         }
-        public void print()
-        {
-            if(this.estado_actual==Estado.vacia
-            {
-            Console.WriteLine("▒");
-            }
-            else if (this.estado_actual== Estado.viva)
-            {
-            Console.WriteLine("▓");
-            }
- 
-        }
-    }
 
-    class Tablero{
-        private list <list<Celula>> grid;
-        public Tablero(short num_renglones, shor num_columnas)
+
+        public void actualiza_estado()
         {
-            grid=new list<list<Celula>>();
-            for(short i = 0 ; i<= num_renglones-1 ; i++) 
-            {
-                grid.Add(new list<Celula>());
-                for(short j = 0 ; j >= num_columnas-1 ; j++)
+            estado_actual=estado_siguiente;
+        }
+
+        public void actualiza_estado_siguiente(){
+        //Actualiza estado_siguiente
+        //Siguiendo las reglas del juego
+
+        //Ejemplo
+        if(num_vecinas() == 0)
+        {
+            estado_siguiente=Estado.viva;
+        }
+        }
+
+        public short num_vecinas()
+        {   short cuenta = 0;
+            // 1 
+            if (renglon > 0  && columna > 0)
                 {
-                 grid[i].Add(new Celula(Estado.viva,this,i,j));
+                  if(  tablero.grid[renglon-1][columna-1].estado_actual == Estado.viva)
+                      cuenta++;
                 }
-            }
+            //falta hacer lo mismo en las otras vecinas
+
+            return cuenta;
+        } 
+
+        public void print(){
+           if (this.estado_actual == Estado.vacia){
+                Console.Write("▒");
+           } 
+           else 
+           {
+                Console.Write("█");
+           }
             
-     
-        }
-
-        public void inserta(Celula c)
-        {
-         grid[c.renglon ][c.columna] = c;
-        }
-
-        
-        public void print()
-        {
-            foreach(list<Celula>renglon in grid)
-            {
-                foreach(Celula c in renglon )
-                  c.print();
-                Console.WriteLine();
-            }
-              
         }
     }
 
+    class Tablero {
+        public List<List<Celula >> grid;
+        public Tablero(short num_renglones, short num_columnas){
+              grid = new List<List<Celula>>(); 
+              for (short i=0; i<= num_renglones-1; i++)
+              {
+                 grid.Add(new List<Celula>()); 
+                 for (short j = 0; j <= num_columnas-1; j++)
+                 {
+                    grid[i].Add(new Celula(Estado.vacia, this, i,j));
+                 }
+              }
+
+        }
+
+       public void actualiza_estado_todas()
+       {
+           foreach(List<Celula> renglon in grid)
+            {
+               foreach(Celula c in renglon)
+               {
+                    c.actualiza_estado();
+                }         
+            }   
+       }
+       //Cambia el estado de todas las celdas
+       
+        public void inserta(Celula c){
+                grid[c.renglon][c.columna] = c;
+        }
+
+        public void print(){
+            foreach(List<Celula> renglon in grid)
+            {
+               foreach(Celula c in renglon)
+               {
+                    c.print();
+                }         
+                Console.WriteLine("");
+            }                  
+        } 
+    }
+    
     class Program
-    {
+{
         static void Main(string[] args)
         {
-         Tablero gol=new Tablero(40,40);
-         gol.inserta(new Celula(Estado.viva,gol,3,3));
-         gol.inserta(new Celula(Estado.viva,gol,3,2));
-         gol.inserta(new Celula(Estado.viva,gol,3,1));
-         gol.inserta(new Celula(Estado.viva,gol,0,0));
-         gol.print();
+             Tablero GoL = new Tablero(10,5);
+             GoL.inserta( new Celula(Estado.viva,GoL, 3,3  ) );
+             GoL.inserta( new Celula(Estado.viva,GoL, 3,2  ) );
+             GoL.inserta( new Celula(Estado.viva,GoL, 3,1  ) );
+             GoL.inserta( new Celula(Estado.viva,GoL, 0,0  ) );
+             
+             GoL.print(); 
+             Console.WriteLine(GoL.grid[1][1].num_vecinas());  
         }
     }
 }
