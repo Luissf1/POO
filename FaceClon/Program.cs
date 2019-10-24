d4c3f93019409adc743a1c23c82c491db27917d5
Luissf1 <luis.silva1252@gmail.com>
2019-10-23 18:49:51 -0700

    update

--- FaceClon/Program.cs
+++ FaceClon/Program.cs
@@ -12,11 +12,12 @@ namespace FaceClon
     protected int fecha;
     
 
-    public Publicacion(string comentario,string reacciones,int fecha)
+    public Publicacion(int fecha,string reacciones,string comentario)
     {
-      this.comentario=comentario;
-      this.reacciones=reacciones;
       this.fecha=fecha;
+      this.reacciones=reacciones;
+      this.comentario=comentario;
+     
     }
 
     public abstract void Imprime();
@@ -26,39 +27,55 @@ namespace FaceClon
  
  class Estado:Publicacion
  {
-public Estado(string comentario,string reaccion,int fecha):base(comentario,reaccion,fecha)
+public Estado(int fecha,string reaccion,string comentario):base(fecha,reaccion,comentario)
 {
 
 }
 
 public override void Imprime()
 {
-    Console.WriteLine("Se publico un estado el {0},recibio de reaccion {1},y comentaron {} ", fecha,reacciones,comentario);
+    Console.WriteLine("Se publico un estado el {0} del mes,recibio de reaccion {1},y comentaron {2} ", fecha,reacciones,comentario);
 }
 
  }
 
 
-/*/ class Foto:Publicacion
+class Foto:Publicacion
 {
+protected string fotografia;
+public Foto(string fotografia,int fecha,string reaccion,string comentario):base(fecha,reaccion,comentario)
+{
+ this.fotografia=fotografia;
+}
+
+public override void Imprime()
+{
+    Console.WriteLine("Se publico una {0} el {1} del mes,recibio de reaccion {2} ,y comentaron {3} ", fotografia,fecha,reacciones,comentario);
+}
 
 }
-class Link:Publicacion
+
+
+/*class Link:Publicacion
 {
 
-}*/
+} */
 
 
     class Program
     {
         static void Main(string[] args)
         {
-        List<Publicacion>Pub=new List<Publicacion>;
-        Pub.Add(new Estado(0,"like","hola"));
-        foreach(var E in Pub)
-        {
-        
+        List<Publicacion> Accion =new List<Publicacion>();
+        Accion.Add(new Estado(22,"like","hola"));
+        Accion.Add(new Foto("foto",14,"me encanta","hola"));
+        foreach(Publicacion a in Accion)
+        { 
+            a.Imprime();
+            
         }
+
+
         }
     }
 }
