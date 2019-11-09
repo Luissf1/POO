07c99bc8fe950ba39748b95ea71be3e3c64c366c
ITT <luis.silva1252@gmail.com>
2019-11-08 20:56:02 -0800

    stack

---
+++ Pila/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Pila
+{
+     //https://sites.google.com/site/ittprogoo/generics
+    public class Pila<T>/*T es de tipo */
+    {
+    readonly int tamaño;
+    int apuntador=0;
+    T[] elementos;
+
+    public Pila(int size)
+    {
+        tamaño=size;
+        elementos=new T[tamaño];
+    }
+    
+
+    public void Push(T elementos)
+    {
+        if(apuntador<tamaño)
+        {
+        elementos[apuntador]=elementos;
+        apuntador++;
+        }
+        else
+        {
+         throw new StackOverflowException();//error
+        }
+    }
+
+    public T Pop()
+    {
+      apuntador--;
+      return elementos[apuntador];
+
+    }
+
+    }
+
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
