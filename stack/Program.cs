609377328df182bf6c4e46fa64a7e1c432a4f811
Luissf1 <luis.silva1252@gmail.com>
2019-11-13 20:05:29 -0800

    pila

---
+++ stack/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace stack
+{   
+    class Stack<T>
+    {
+     private T[] elementos;
+     readonly int max_size;
+     private int apuntador_pila=0;
+
+     public Stack():this(100)
+     {     }
+
+
+     public Stack(int size)
+     {
+         max_size=size;
+         elementos=new T[max_size];
+     }
+
+     public void Push(T elemento)
+     {
+      elementos[apuntador_pila]=elemento;
+      apuntador_pila++;
+     }  
+
+     public T Pop()
+     {
+         apuntador_pila--;
+         return elementos[apuntador_pila];
+     }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> nombres= new Stack<string>(4); 
+            nombres.Push("Mundo");
+            nombres.Push(" ");
+            nombres.Push("Hola");
+            
+            nombres.Push("Mundo");
+            nombres.Push(" ");
+            nombres.Push("Hola");
+
+            string nombre=nombres.Pop();
+            Console.Write(nombre);
+            Console.Write(nombres.Pop());
+            Console.Write(nombres.Pop());
+            //Console.Write(nombres.Pop());
+        }
+    }
+}
