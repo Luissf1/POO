7743f853e3c533de04fe135c822863ced0389e5e
ITT <luis.silva1252@gmail.com>
2019-11-19 12:57:30 -0800

    cambios

--- creadir/Program.cs
+++ creadir/Program.cs
@@ -8,15 +8,18 @@ namespace creadir
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Captura el folder a crear");
+            Console.WriteLine("Captura el archivo a ocultar");
 
-            string directorio=Console.ReadLine();
+            //string directorio=Console.ReadLine();
+            string archivo=Console.ReadLine();
+            File.SetAttributes(archivo,FileAttributes.Hidden); 
+
            /* if(Directory.Exists(directorio))
             {
              Console.WriteLine("Ya existe directorio");
             }
             else{*/
-            Directory.Delete(directorio,true);
+           // Directory.Delete(directorio,true);
            /* Directory.CreateDirectory(directorio);
             Console.WriteLine("Directorio creado");*/
             /*}*/
