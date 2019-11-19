a48ebedfe0915d15b1e74cc0b723d7cf609b2168
ITT <luis.silva1252@gmail.com>
2019-11-19 12:34:59 -0800

    Trabajo clases martes 19/11/2019

---
+++ creadir/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace creadir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Captura el folder a crear");
+
+            string directorio=Console.ReadLine();
+            if(Directory.Exists(directorio))
+            {
+             Console.WriteLine("Ya existe directorio");
+            }
+            else{
+            Directory.CreateDirectory(directorio);
+            Console.WriteLine("Directorio creado");
+            }
+            
+
+        }
+    }
+}
