689d182da89510d36f08461260208dc276bda85f
Luissf1 <luis.silva1252@gmail.com>
2019-11-19 20:27:41 -0800

    Tarea

--- Cola/Program.cs
+++ Cola/Program.cs
@@ -1,83 +1,37 @@
 ﻿using System;
+using System.Collections;
 
 
 namespace Cola
 {
-    class Cola<C>//Creacion de clase 
-    {
-     //Declaracion de variable
-     private C[] elemento;
-     readonly int size_max;
-     private int pointer=0;
-
-     //Creacion de constructor
-     public Cola():this(100)
-     {
-
-     }
-
-     //Sobre carga del constructor
-     public Cola(int size)
-     {
-         //Asignacion de size
-         size_max=size;
-         //Asignacion de size a elemento
-         elemento=new C[size_max];
-     }
-     
-     //Creacion de push ingresa los valores al stack
-     public void Push(C elementos)
-     {
-         //creacion de un if para una excepcion
-         if(pointer>=size_max)
-         {
-            throw new StackOverflowException();
-         }
-         else{
-         //permite que los elementos ingresen al stack
-         elemento[pointer]=elementos;
-         //permite el subir un nivel del Stack ,empezando de 0 y sube a 1
-         pointer++;
-         }
-     }  
-     
-     //Creacion del pop ,saca los elementos de stack
-     public C Pop()
-     {
-         //Permite bajar un nivel del stack
-         pointer--;
-         //Creacion de if para excepcion de Pop
-         if(pointer>=0)
-         {
-         //regresa el valor del elemento sacado
-         return elemento[pointer];
-         }
-         else{
-             pointer=0;
-             throw new InvalidOperationException("Error stack contiene espacio vacio");
-         }
-     }
-
-    }
+  
     class Program
     {
         static void Main(string[] args)
         {
-             //Creacion de objeto tipo Cola llamado numeros
-            Cola<int> numero= new Cola<int>(4); 
-            //push de numeros
-            numero.Push(1);
-            numero.Push(2);
-            numero.Push(3);
-          
-           for (int i=0; i<3; i++) 
-        {
-            int numeros;
-            numeros =(int) numero.Pop();
-            Console.WriteLine(numeros);
-           
+        //Creacion de string Nombre
+         string Nombre;
+         
+         //Queje es la clase creada apartir de utilizar el System.Collections
+         Queue Cola =new Queue();//Queje es la clase creada apartir de utilizar el System.Collections
+         
+         //Enqueje permite que se inroduzcan los valores atraves de la cabeza
+         Cola.Enqueue("Silva ");
+         Cola.Enqueue("Reyes,");
+         Cola.Enqueue("Luis ");
+         Cola.Enqueue("Adrian");
+         
+         //Creamos un for para poder imprimir los 4 valores ingresados
+         for(int i=0;i<4;i++)
+         {
+        //Declaramos a nombre como el Dequeue del objeto Cola
+        //Dequeue es el que permite extrear los valores por el extremo opuesto
+         Nombre=(string) Cola.Dequeue();
+         //Imprimimos Nombre
+         Console.Write(Nombre);
+         //Se utilizo la regla de FiFo ,First in ,First out
+         //Enqueje es el encolar y Dequeue es el desencolar 
          }
-           
         }
     }
 }
