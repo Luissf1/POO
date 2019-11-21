9a61234cf0b10b31531eea0e8cb3a3cbb8d63854
ITT <luis.silva1252@gmail.com>
2019-11-21 12:47:56 -0800

    Practica clase

--- creadir/Program.cs
+++ creadir/Program.cs
@@ -1,20 +1,50 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace creadir
 {
+    class Producto
+    {
+     public string code;
+     public string descripcion;
+     public double precio;
+
+     public Producto(string c,string d,double p)
+     {
+       this.code=c;
+       this.descripcion=d;
+       this.precio=p;
+     }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            List<Producto> productos=new List<Producto>();
+            productos.Add(new Producto("Bic","Lapiz Azul N2",12.23d));
+            productos.Add(new Producto("Bic","Lapiz rojo N2",12.23d));
+            productos.Add(new Producto("Bic","Pluma verde",12.23d));
+            productos.Add(new Producto("Bic","Pluma negra",12.23d));
+            
+            FileStream fs=new FileStream(@"productos.tx",FileMode.OpenOrCreate,FileAccess.Write);
+            StreamWriter txtout =new StreamWriter(fs);
+            
+            foreach(Producto p in productos)
+            {
+                //txtout.Write(p.descripcion+"");
+                txtout.WriteLine("{0}|{1}|{2}",p.code,p.descripcion,p.precio);
+            }
+            txtout.Close();
+            Console.ReadKey();
 
-            Console.WriteLine("Captura el archivo a ocultar");
-
-            //string directorio=Console.ReadLine();
+           /* Console.WriteLine("Captura el archivo a ocultar");
             string archivo=Console.ReadLine();
             File.SetAttributes(archivo,FileAttributes.Hidden); 
-
-           /* if(Directory.Exists(directorio))
+            string directorio=Console.ReadLine();
+            if(Directory.Exists(directorio))
             {
              Console.WriteLine("Ya existe directorio");
             }
@@ -23,8 +53,6 @@ namespace creadir
            /* Directory.CreateDirectory(directorio);
             Console.WriteLine("Directorio creado");*/
             /*}*/
-            
-
         }
     }
 }
