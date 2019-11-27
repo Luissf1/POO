fb2340ffe8be7b4f035e1cfe26e66c84bff9f5b3
ITT <luis.silva1252@gmail.com>
2019-11-27 12:30:39 -0800

    update

--- creadir/Program.cs
+++ creadir/Program.cs
@@ -18,6 +18,21 @@ namespace creadir
      }
     }
 
+    class ProductoArchivo
+    {
+     public static void EscribeProductos(string archivo,List<Producto> productos)
+     {
+        FileStream fs=new FileStream(archivo,FileMode.OpenOrCreate,FileAccess.Write);
+        StreamWriter txtout =new StreamWriter(fs);
+            
+        foreach(Producto p in productos)
+         {
+            txtout.WriteLine("{0}|{1}|{2}",p.code,p.descripcion,p.precio);
+          }
+            txtout.Close();
+     }
+    }
+
 
     class Program
     {
@@ -29,21 +44,17 @@ namespace creadir
             productos.Add(new Producto("Bic","Lapiz rojo N2",12.23d));
             productos.Add(new Producto("Bic","Pluma verde",12.23d));
             productos.Add(new Producto("Bic","Pluma negra",12.23d));
+
+            ProductoArchivo.EscribeProductos(@"productos.tx");
             
-            FileStream fs=new FileStream(@"productos.tx",FileMode.OpenOrCreate,FileAccess.Write);
-            StreamWriter txtout =new StreamWriter(fs);
-            
-            foreach(Producto p in productos)
-            {
-                txtout.WriteLine("{0}|{1}|{2}",p.code,p.descripcion,p.precio);
-            }
-            txtout.Close();
-            Console.ReadKey();
+           
             Console.WriteLine("Archivo grabado");
 
             List<Producto> productos_leidos =new List<Producto>();
 
-            using(StreamReader sr=new StreamReader(@"productos.tx"))
+           
+
+            using(StreamReader sr=new StreamReader(@"productos.tx"))//se utiliza using para no utilizar el close
             {
               string line ="";
               while((line= sr.ReadLine()) != null )//No lleguemos al final del archivo
