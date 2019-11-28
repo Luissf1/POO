062d6400400e44ea3a05eaabbea0332dbac52967
Luissf1 <luis.silva1252@gmail.com>
2019-11-28 09:06:55 -0800

    proyecto

---
+++ ProyectoFinal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProyectoFinal
+{
+    class Producto
+    {
+     public string Codigo;
+     public string Descripcion;
+     public decimal Precio;
+     public int Departamento;
+     public int Likes;
+
+     public void Producto(string Codigo,string Descripcion,decimal Precio,int Departamento,int Likes)
+     {
+
+     }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
