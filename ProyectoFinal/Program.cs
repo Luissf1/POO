d8630ed343069cd97abc88ede921b61b9ee431d5
Luissf1 <luis.silva1252@gmail.com>
2019-12-02 17:27:51 -0800

    update

--- ProyectoFinal/Program.cs
+++ ProyectoFinal/Program.cs
@@ -28,7 +28,72 @@ namespace ProyectoFinal
     
     class ProductoDB
     {
+    public static void ProductosTXTOUT(string datos,List<Producto> productos)
+    {
+     FileStream FileS1=new FileStream(datos,FileMode.OpenOrCreate,FileAccess.Write);
+        StreamWriter TXTOUT =new StreamWriter(FileS1);
+            
+        foreach(Producto p in productos)
+        {
+         TXTOUT.WriteLine("{0}|{1}|{2}|{3}|{4}",p.Codigo,p.Descripcion,p.Precio,p.Departamento,p.Likes);
+        }
+        TXTOUT.Close(); 
+    }
 
+    public static void ProductosBINOUT(string datos,List<Producto> productos)
+    {
+    FileStream FileS2=new FileStream(datos,FileMode.OpenOrCreate,FileAccess.Write);    
+         BinaryWriter binOut=new BinaryWriter(FileS2);
+         foreach(Producto pb in productos)
+         {
+          binOut.Write(pb.Codigo);
+          binOut.Write(pb.Descripcion);
+          binOut.Write(pb.Precio);
+          binOut.Write(pb.Departamento);
+          binOut.Write(pb.Likes);
+         }
+        binOut.Close();
+    }
+
+    public static List<Producto> ProductosTXTIN(string datos)
+    {
+    StreamReader TXTIN=new StreamReader(new FileStream(datos,FileMode.OpenOrCreate,FileAccess.Write));    
+    List<Producto> productos =new List<Producto>();
+    string row="";    
+    while (TXTIN.Peek()!= -1)
+    {
+       string fila=TXTIN.ReadLine();
+       string[] columna= row.Split('|');
+       Producto producto =new Producto();
+       producto.Codigo= columna[0];
+       producto.Descripcion= columna[1];
+       producto.Precio= Convert.ToDouble(columna[2]);
+       producto.Departamento= columna[3];
+       producto.Likes=Convert.ToInt32(columna[4]);
+       productos.Add(producto);
+    }
+    TXTIN.Close();
+
+    return productos;
+
+    }
+
+    public static List<Producto> ProductosBININ(string datos)
+    {
+    BinaryReader BININ=new BinaryReader(new FileStream(datos,FileMode.OpenOrCreate,FileAccess.Write));    
+    List<Producto> productos =new List<Producto>();
+    while (BININ.PeekChar()!= -1)
+    {
+    Producto producto =new Producto();
+    producto.Codigo= BININ.ReadString();
+    producto.Descripcion= BININ.ReadString();
+    producto.Precio= BININ.ReadDouble();
+    producto.Departamento= BININ.ReadString();
+    producto.Likes= BININ.ReadInt32();
+    productos.Add(producto);
+    } 
+    BININ.Close();
+    }
 
     }
 
@@ -43,28 +108,13 @@ namespace ProyectoFinal
         productos.Add(new Producto("3","Lapiz Bic",4.42d,"Papeleria",60));
         productos.Add(new Producto("4","Libreta Norma",11.54d,"Papeleria",70));
 
+        ProductoDB.ProductosTXTOUT(@"Producto.bin",productos);
 
-        FileStream FS=new FileStream(@"Producto.txt",FileMode.OpenOrCreate,FileAccess.Write);
-        StreamWriter TXTOUT =new StreamWriter(FS);
-            
-        foreach(Producto p in productos)
+        List<Producto> ProductosINB= ProductoDB.ProductosBININ("Producto.bin");
+        foreach(Producto PB in ProductosINB)
         {
-            TXTOUT.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}",p.Codigo,p.Descripcion,p.Precio,p.Departamento,p.Likes);
+            Console.WriteLine("{0},{1},{2},{3},{4}",PB.Codigo,PB.Descripcion,PB.Precio,PB.Departamento,PB.Likes);
         }
-            TXTOUT.Close(); 
-
-
-         FileStream FS2=new FileStream(@"Producto.bin",FileMode.OpenOrCreate,FileAccess.Write);    
-         BinaryWriter binOut=new BinaryWriter(FS2);
-         foreach(Producto pb in productos)
-         {
-          binOut.Write(pb.Codigo);
-          binOut.Write(pb.Descripcion);
-          binOut.Write(pb.Precio);
-          binOut.Write(pb.Departamento);
-          binOut.Write(pb.Likes);
-         }
-         binOut.Close();
         }
     }
 }
