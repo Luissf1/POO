f3d62d1d4a0126d9d6f251fff11487136b6dcba2
Luissf1 <luis.silva1252@gmail.com>
2019-12-10 10:50:45 -0800

    update

--- ProyectoFinal/Program.cs
+++ ProyectoFinal/Program.cs
@@ -140,36 +140,6 @@ namespace ProyectoFinal
             Console.WriteLine("{0} {1} {2} {3} {4}", p.Codigo, p.Descripcion,p.Precio, p.Departamento,p.Likes);
         }
 
-    /*string eleccion = Console.ReadLine();
-    int depo=Convert.ToInt32(eleccion);
-    
-     switch (depo)
-     {
-      case 1:
-       Console.WriteLine("1|Libreta Scribe|40.52|Departamento 1|40");
-      break;
-
-      case 2:
-       Console.WriteLine("2|Pluma Negra Bic|5.53|Departamento 2|50");      
-      break;
-
-      case 3:
-       Console.WriteLine("3|Lapiz Bic|4.42|Departamento 3|60");
-      break;
-
-      case 4:
-       Console.WriteLine("4|Libreta Norma|11.54|Departamento 4|70");
-      break;
-
-      case 5:
-       Console.WriteLine("Salida");
-      break;
-
-      default:
-      Console.WriteLine("Fin de lista");
-      break;
-      
-     }*/
     }
 
     public void OrdenarLikes()
@@ -195,45 +165,41 @@ namespace ProyectoFinal
         static void Main(string[] args)
         {
         List<Producto> productos =new List<Producto>();
+        //Lista para poder ordenar por likes 
         ProductoDB pro=new ProductoDB();
         pro.productos.Add(new Producto("1","Libreta Scribe",40.52d,1,50));
         pro.productos.Add(new Producto("2","Pluma Negra Bic",5.53d,2,30));
         pro.productos.Add(new Producto("3","Lapiz Bic",4.42d,3,60));
         pro.productos.Add(new Producto("4","Libreta Norma",11.54d,4,20));
-
+        //lista de productos para imprimir en txt y bin
         productos.Add(new Producto("1", "Libreta Scribe", 40.52d, 1, 50));
         productos.Add(new Producto("2", "Pluma Negra Bic", 5.53d, 2, 30));
         productos.Add(new Producto("3", "Lapiz Bic", 4.42d, 3, 60));
         productos.Add(new Producto("4", "Libreta Norma", 11.54d, 4, 20));
-
+        
+        //permite imprimir el bin y crear el archivo
         ProductoDB.ProductosBINOUT(@"Producto.bin",productos);
         Console.WriteLine("Datos guardados en formato bin");
         Console.WriteLine("--------------------------------");
-
         List<Producto> ProductosINB = ProductoDB.ProductosBININ(@"Producto.bin");
         foreach(Producto PB in ProductosINB)
         {
             Console.WriteLine("{0} {1} {2} {3} {4}",PB.Codigo,PB.Descripcion,PB.Precio,PB.Departamento,PB.Likes);
         }
-
+        
+        //permite imprimir el txt y crear el archivo
         ProductoDB.ProductosTXTOUT(@"Producto.txt", productos);
         Console.WriteLine("Datos guardados en formato txt");
         Console.WriteLine("--------------------------------");
-        
         List<Producto> ProductoINT = ProductoDB.ProductosTXTIN(@"Producto.txt");
         foreach(Producto PT in ProductoINT)
         {
          Console.WriteLine("{0} {1} {2} {3} {4}",PT.Codigo,PT.Descripcion,PT.Precio,PT.Departamento,PT.Likes);
         }
 
-        /*Console.WriteLine("Escoge un Departamento");
-        Console.WriteLine("--------------------------------");
-        Console.WriteLine("1)Departamento 1");
-        Console.WriteLine("2)Departamento 2");
-        Console.WriteLine("3)Departamento 3");
-        Console.WriteLine("4)Departamento 4");
-        Console.WriteLine("5)Salir");*/
+        //Permite que obtengas el departamento ingresado ,en este caso 4 
         pro.GetDepartment(4);
+        //Permite ordenar los 
         pro.OrdenarLikes();
 
         }
